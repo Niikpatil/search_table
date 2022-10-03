c5361e86fe419a8843e7db985d19da446a3a3615
Nik Patil <nikpatildev@protonmail.com>
2022-10-03 12:57:07 +0530

    render boolean value table
    status boolean logic working

--- src/components/DataList/UserList.js
+++ src/components/DataList/UserList.js
@@ -1,12 +1,20 @@
 import React from "react";
 
 const UserList = ({ itemList }) => {
+  let status;
+
+  if (itemList.completed == true) {
+    status = "true";
+  } else {
+    status = "False";
+  }
+
   return (
     <tbody>
       <tr>
         <td>{itemList.id}</td>
         <td>{itemList.title}</td>
-        <td>{itemList.id}</td>
+        <td>{status}</td>
         <td>
           <button type="button" className="btn btn-sm btn-warning">
             View
